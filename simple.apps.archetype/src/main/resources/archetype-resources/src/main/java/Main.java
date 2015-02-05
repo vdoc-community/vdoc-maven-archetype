7ae6fc9a4fc8481160f38641971c7e782accc15d
famaridon <famaridon@gmail.com>
2015-02-05 21:21:48 +0100

    import

---
+++ simple.apps.archetype/src/main/resources/archetype-resources/src/main/java/Main.java
@@ -0,0 +1,10 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+/**
+ * Created by famaridon on 23/01/15.
+ */
+public class Main {
+}
